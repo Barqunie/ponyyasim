f922510ffd5c6122b8294293f50175ff85b3ddaf
Barış Ayık <118805901+Barqunie@users.noreply.github.com>
2023-07-02 23:27:54 +0300

    Add files via upload

---
+++ aa.js
@@ -0,0 +1,32 @@
+
+function hesapla(){
+const x = document.getElementById('yas').value;
+document.getElementById("sonuc").innerHTML = Math.floor( x*99.45);
+console.log(val);}
+
+var audio = document.getElementById('background_audio');
+audio.volume = 0.1;
+
+
+document.getElementById('sus').addEventListener('click', function (e)
+{
+    e = e || window.event;
+    audio.muted = !audio.muted;
+    e.preventDefault();
+}, false);
+
+
+
+const img= document.getElementById('sus');
+
+let toggle = true;
+
+img.addEventListener('click',function(){
+    toggle = !toggle;
+    if (toggle) {
+        
+        img.src = 'img\\mute.png';
+    }
+    else{img.src = 'img\\muted.png';}
+
+} );
